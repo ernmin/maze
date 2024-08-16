d527b9bae2c9f0964a956743924e732ad8931c13
ernmin <42770608+ernmin@users.noreply.github.com>
2024-08-16 13:23:37 +0800

    Add structure files for Maze App

---
+++ app.py
@@ -0,0 +1,23 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL("sqlite:///history.db")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#do a login like finance app but using flash instead of apology()
+
+
+#decide what sql database will be like
+
+#index page to display moves and add moves, like birthday app.py
