9a9d80411aee54c1407a767eeffeab2024a23807
ernmin <42770608+ernmin@users.noreply.github.com>
2024-08-18 02:01:28 +0800

    Add Login Page Resolve Error in layout template. Username form did not display. Change Database file to Maze.db Add Users Table in Maze.db

--- app.py
+++ app.py
@@ -9,11 +9,43 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///history.db")
+db = SQL("sqlite:///maze.db")
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return flash("must provide username")
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 :
+            return flash("invalid username")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 #do a login like finance app but using flash instead of apology()
 
